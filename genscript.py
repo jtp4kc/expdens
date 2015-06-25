f97bfcc1c2185af5197373c484d45d2e99270358
Tyler P <jtp4kc@virginia.edu>
2015-06-24 23:49:55 -0400

    Merged the two generator script files

--- genscript.py
+++ genscript.py
@@ -5,85 +5,101 @@ Created on Jun 19, 2015
 '''
 import sys, os
 
-def place_vars(base, suffix, dir):
-    text_ = """#!/bin/sh
-        #SBATCH --job-name=%s%s
-        """ % (base, suffix)
-    text_ = text_ + """#SBATCH --partition=serial
-        #SBATCH --nodes=1
-        #SBATCH --ntasks=10
-        #SBATCH --time=2-06:00:00
-        #SBATCH --signal=15 --comment="15 = SIGTERM"
-        """
-    text_ = text_ + """#SBATCH --output=%s.out
-        """ % (os.path.join(dir, base + suffix, base))
-    text_ = text_ + """#SBATCH --workdir=%s
-        """ % (os.path.join(dir, base + suffix))
-    text_ = text_ + """#SBATCH --checkpoint=06:00:00
-        #SBATCH --checkpoint-dir=/scratch/jtp4kc/checkpoints
-        #SBATCH --mail-type=ALL
-        #SBATCH --mail-user=jtp4kc@virginia.edu
-        """
-#    text_ = text_ + """#SBATCH --comment="#SBATCH --array-0,1,3" """
-    text_ = text_ + """
-        # submit a single SLURM job for a free energy calc
-        module load gromacs/5.0.2-sse
-        export THREADINFO="-nt 10 "
-        export GMX_SUPPRESS_DUMP=1 #prevent step file output when parameters don't converge
-        
-        #export OMP_NUM_THREADS=10
-        
-        sleep 1
-        
-        #Change the job status to 'SUBMITTED'
-        echo "SUBMITTED %s" >> ../jobstatus.txt
-        """ % suffix
-    text_ = text_ + """echo "Job Started at `date`"
-        
-        #NVT PRODUCTION
-        """
-    text_ = text_ + """INFOLDER="../%s-in/"
-        GNAME="%s"
-        """ % (base, base)
-    text_ = text_ + """GNAMEIN="${INFOLDER}${GNAME}"
-        
-        cp ${GNAMEIN}%s-in.gro ${GNAME}-in.gro
-        """ % (suffix)
-    text_ = text_ + """cp ${GNAMEIN}.top ${GNAME}.top
-        cp ${GNAMEIN}.ndx ${GNAME}.ndx
-        #avoid overwriting file
-        if [ ! -f ${GNAME}.mdp ];
-        then
-            cp ${GNAMEIN}.mdp ${GNAME}.mdp
-        fi
-        
-        grompp_d -c ${GNAME}-in.gro -p ${GNAME}.top -n ${GNAME}.ndx -f ${GNAME}.mdp -o ${GNAME}.tpr -maxwarn 15
-        mdrun_d ${THREADINFO} -deffnm ${GNAME}
-        
-        echo "FINISHED %s" >> ../jobstatus.txt
-        """ % suffix
-    text_ = text_ + """# print end time
-        echo
-        echo "Job Ended at `date`"
-        echo "###################################################################"
-        """
-    text = ''
-    for line in text_.splitlines():
-        text = text + line.lstrip() + '\n'
+def place_vars(fields):
+    fields['outputfile'] = os.path.join(fields['path'], fields['base'] +
+        fields['suffix'], fields['base'])
+    fields['workdir'] = os.path.join(fields['path'], fields['base'] +
+        fields['suffix'])
+    fields['infolder'] = os.path.join('..', fields['base'] + '-in', '')
+    fields['jobstatus'] = os.path.join('..', 'jobstatus.txt')
     
-    return text
+    text_ = """#!/bin/sh
+#SBATCH --job-name={base}{suffix}
+#SBATCH --partition=serial
+#SBATCH --nodes=1
+#SBATCH --ntasks={ntasks}
+#SBATCH --time=2-06:00:00
+#SBATCH --signal=15 --comment="15 = SIGTERM"
+#SBATCH --output={outputfile}.out
+#SBATCH --workdir={workdir}
+#SBATCH --checkpoint=06:00:00
+#SBATCH --checkpoint-dir=/scratch/jtp4kc/checkpoints
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jtp4kc@virginia.edu
+#SBATCH --comment="#SBATCH --array-0,1,3"
 
-def generate(base_name, submit):
-    dir = '/scratch/jtp4kc/simulations/t41meth'
-#    dir = os.getcwd()
+# submit a single SLURM job for a free energy calc
+module load gromacs/5.0.2-sse
+export THREADINFO="-nt {ntasks} "
+export GMX_SUPPRESS_DUMP=1 #prevent step file output when parameters don't converge
+
+#export OMP_NUM_THREADS={ntasks}
+
+sleep 1
+
+#Change the job status to 'SUBMITTED'
+echo "SUBMITTED {suffix}" >> {jobstatus}
+echo "Job Started at `date`"
+
+#NVT PRODUCTION
+cp {infolder}{gro-in}-in.gro {base}-in.gro
+cp {infolder}{base}.top {base}.top
+cp {infolder}{base}.ndx {base}.ndx
+#avoid overwriting file
+if [ ! -f {base}.mdp ];
+then
+    cp {infolder}{mdp-in}.mdp {base}.mdp
+fi
+
+grompp_d -c {base}-in.gro -p {base}.top -n {base}.ndx -f {base}.mdp -o {base}.tpr -maxwarn 15
+mdrun_d ${{THREADINFO}} -deffnm {base}
+
+echo "FINISHED {suffix}" >> {jobstatus}
+# print end time
+echo
+echo "Job Ended at `date`"
+echo "###################################################################"
+""".format(**fields)
+    
+    return text_
+
+def generate(base_name, submit, randseed):
+#    path = '/scratch/jtp4kc/simulations/t41meth'
+    path = os.getcwd()
     for i in range(20):
         suffix = '_{0:0>2}'.format(i)
+        fields = dict()
+        fields['base'] = base_name
+        fields['suffix'] = suffix
+        fields['path'] = path
+        fields['ntasks'] = 10
+        fields['gro-in'] = base_name + suffix
+        fields['mdp-in'] = base_name
+        if randseed:
+            fields['gro-in'] = base_name
+            fields['mdp-in'] = base_name + suffix
+            try: 
+                mdp_out = open(os.path.join(path, base_name + "-in", 
+                    base_name + suffix + ".mdp"), 'w')
+                for line in open(os.path.join(path, base_name + "-in", 
+                    base_name + ".mdp")):
+                    
+                    if 'seed' in line:
+                        halves = line.split("=")
+                        num = int(halves[1].strip())
+                        num += i
+                        line = halves[0] + "= " + str(num) + "\n"
+                    mdp_out.write(line)
+                mdp_out.close()
+            except IOError as ioex:
+                print(ioex)
+        
         file_name = base_name + suffix + '.slurm'
         file_ = open(file_name, 'w')
-        file_.write(place_vars(base_name, suffix, dir))
+        file_.write(place_vars(fields))
         file_.close()
         if submit:
-            dir_name = os.path.join(dir, base_name + suffix)
+            dir_name = os.path.join(path, base_name + suffix)
             if not os.path.exists(dir_name):
                 os.mkdir(dir_name)
             os.system("sbatch " + file_name)
@@ -106,7 +122,7 @@ def main(argv=None):
 #
 #    args = parser.parse_args(argv)
     
-    generate("t41meth", True)
+    generate("t41meth", False, False)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
